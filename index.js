7d9e040d3c24caa36954e24f0d0aa8e0fbd12a72
agdavid <antonio.g.david@gmail.com>
2019-03-12 22:34:57 -0400

    revert to vanilla JS, without alexa-sdk-core

--- index.js
+++ index.js
@@ -1,108 +1,183 @@
-const Alexa = require('ask-sdk-core');
+let intentHandlers = {};
 
-const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-      const speechText = 'Welcome to the Pokedex!';
-      speechText += 'You can name a pokemon to get it\`s facts.';
-      speechText += 'For example you can say, find me Jigglypuff.';
-  
-      const repromptText = 'Which pokemon would you like to learn about?';
-      repromptText += 'For example you can say, find me Jigglypuff.';
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(repromptText)
-        .withSimpleCard('Pokedex', speechText)
-        .getResponse();
+exports.handler = function (event, context) {
+  try {
+    
+      if (!event.session.attributes) {
+          event.session.attributes = {};
+      }
+
+      if (event.request.type === 'LaunchRequest') {
+          onLaunch(event.request, event.session, new Response(context,event.session));
+      } else if (event.request.type === 'IntentRequest') {
+          var response =  new Response(context,event.session);
+          if (event.request.intent.name in intentHandlers) {
+            intentHandlers[event.request.intent.name](event.request, event.session, response, getSlots(event.request));
+          } else {
+            response.speechText = 'Unknown intent';
+            response.shouldEndSession = true;
+            response.done();
+          }
+      } else if (event.request.type === 'SessionEndedRequest') {
+          context.succeed();
+      }
+  } catch (e) {
+      context.fail('Exception: ' + getError(e));
+  }
+};
+
+function getSlots(req) {
+  let slots = {}
+  for(var key in req.intent.slots) {
+    if(req.intent.slots[key].value !== undefined) {
+      slots[key] = req.intent.slots[key].value;
+    }
+  }
+  return slots;
+}
+
+var Response = function (context,session) {
+  this.speechText = '';
+  this.shouldEndSession = true;
+  this.ssmlEn = true;
+  this._context = context;
+  this._session = session;
+
+  this.done = function(options) {
+
+    if(options && options.speechText) {
+      this.speechText = options.speechText;
+    }
+
+    if(options && options.repromptText) {
+      this.repromptText = options.repromptText;
+    }
+
+    if(options && options.ssmlEn) {
+      this.ssmlEn = options.ssmlEn;
+    }
+
+    if(options && options.shouldEndSession) {
+      this.shouldEndSession = options.shouldEndSession;
+    }
+
+    this._context.succeed(buildAlexaResponse(this));
+  }
+
+  this.fail = function(msg) {
+    this._context.fail(msg);
+  }
+
+};
+
+function createSpeechObject(text,ssmlEn) {
+  if(ssmlEn) {
+    return {
+      type: 'SSML',
+      ssml: '<speak>'+text+'</speak>'
+    }
+  } else {
+    return {
+      type: 'PlainText',
+      text: text
+    }
+  }
+}
+
+function buildAlexaResponse(response) {
+  var alexaResponse = {
+    version: '1.0',
+    response: {
+      outputSpeech: createSpeechObject(response.speechText,response.ssmlEn),
+      shouldEndSession: response.shouldEndSession
     }
   };
 
-  const FactRequestIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'FactRequestIntent';
-    },
-    handle(handlerInput) {
-      const speechText = 'You\'ve requested a pokemon!';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .withSimpleCard('Pokedex', speechText)
-        .getResponse();
-    },
-  };
+  if(response.repromptText) {
+    alexaResponse.response.reprompt = {
+      outputSpeech: createSpeechObject(response.repromptText,response.ssmlEn)
+    };
+  }
 
-  const HelpIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-      const speechText = 'You can name a pokemon to get it\`s facts.';
-      speechText += 'For example you can say, find me Jigglypuff.';
+  if(response.cardTitle) {
+    alexaResponse.response.card = {
+      type: 'Simple',
+      title: response.cardTitle
+    };
+
+    if(response.imageUrl) {
+      alexaResponse.response.card.type = 'Standard';
+      alexaResponse.response.card.text = response.cardContent;
+      alexaResponse.response.card.image = {
+        smallImageUrl: response.imageUrl,
+        largeImageUrl: response.imageUrl
+      };
+    } else {
+      alexaResponse.response.card.content = response.cardContent;
+    }
+  }
+
+  if (!response.shouldEndSession && response._session && response._session.attributes) {
+    alexaResponse.sessionAttributes = response._session.attributes;
+  }
+  return alexaResponse;
+}
+
+function getError(err) {
+  var msg='';
+  if (typeof err === 'object') {
+    if (err.message) {
+      msg = ': Message : ' + err.message;
+    }
+    if (err.stack) {
+      msg += '\nStacktrace:';
+      msg += '\n====================\n';
+      msg += err.stack;
+    }
+  } else {
+    msg = err;
+    msg += ' - This error is not object';
+  }
+  return msg;
+}
+
+function onLaunch(launchRequest, session, response) {
+  response.speechText = 'Hi, welcome to the Pokemon Lookup. You can ask me to find a pokemon. Which pokemon would you like to find?';
+  response.repromptText = 'For example, you can say find Jigglypuff.';
+  response.shouldEndSession = false;
+  response.done();
+}
+
+intentHandlers['GetPokemonInfo'] = function(request,session,response,slots) {
+
+  if(slots.PokemonName === undefined) {
+    response.speechText = 'Looks like you forgot to mention a pokemon. Which pokemon would you like to find? ';
+    response.repromptText = 'For example, you can say find Jigglypuff. ';
+    response.shouldEndSession = false;
+    response.done();
+    return;
+  }
+
+  response.cardTitle = `Pokemon Lookup results for: ${slots.PokemonName}`;
+  response.cardContent = '';
   
-      const repromptText = 'Which pokemon would you like to learn about?';
-      repromptText += 'For example you can say, find me Jigglypuff.';
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(repromptText)
-        .withSimpleCard('Pokedex', speechText)
-        .getResponse();
-    },
-  };
-  
-  const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-          || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-      const speechText = 'Goodbye!';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .withSimpleCard('Pokedex', speechText)
-        .getResponse();
-    },
-  };
-  
-  const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-      console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
-  
-      return handlerInput.responseBuilder.getResponse();
-    },
-  };
-  
-  const ErrorHandler = {
-    canHandle() {
-      return true;
-    },
-    handle(handlerInput, error) {
-      console.log(`Error handled: ${error.message}`);
-  
-      return handlerInput.responseBuilder
-        .speak('Sorry, I can\'t understand the command. Please say again.')
-        .reprompt('Sorry, I can\'t understand the command. Please say again.')
-        .getResponse();
-    },
-  };
-  
-  const skillBuilder = Alexa.SkillBuilders.custom();
-  
-  exports.handler = skillBuilder
-    .addRequestHandlers(
-      LaunchRequestHandler,
-      FactRequestIntentHandler,
-      HelpIntentHandler,
-      CancelAndStopIntentHandler,
-      SessionEndedRequestHandler
-    )
-    .addErrorHandlers(ErrorHandler)
-    .lambda();
+  response.speechText = `You\'ve requested ${slots.PokemonName}! `;
+  response.cardContent += response.speechText;
+  response.shouldEndSession = true;
+  response.done();
+}
+
+intentHandlers['AMAZON.StopIntent'] = function(request,session,response,slots) {
+  response.speechText  = `Good Bye. `;
+  response.shouldEndSession = true;
+  response.done();
+};
+
+intentHandlers['AMAZON.CancelIntent'] =  intentHandlers['AMAZON.StopIntent'];
+
+intentHandlers['AMAZON.HelpIntent'] = function(request,session,response,slots) {
+  response.speechText = 'You can ask me to find a pokemon. Which pokemon would you like to find?';
+  response.repromptText = 'For example, you can say find Jigglypuff.';
+  response.shouldEndSession = false;
+  response.done();
+}
--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 \rm -rf lambda_upload.zip
 zip -r lambda_upload.zip index.js node_modules
-aws lambda update-function-code --function-name Pokedex --zip-file fileb://lambda_upload.zip
+aws lambda update-function-code --function-name PokemonLookup --zip-file fileb://lambda_upload.zip
